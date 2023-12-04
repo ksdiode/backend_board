0f47df24f4294bb60da9f368e112a8d746812bb5
gusukim <gusukim@gmail.com>
2023-12-04 14:21:03 +0900

    프로즈젝트 생성

---
+++ src/main/java/org/scoula/config/ServletConfig.java
@@ -0,0 +1,48 @@
+package org.scoula.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.io.IOException;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "org.scoula.exception",
+        "org.scoula.controller"
+})
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        registry.viewResolver(bean);
+    }
+
+    //	Servlet 3.0 파일 업로드 사용시
+	@Bean
+	public MultipartResolver multipartResolver() {
+	 	StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+		return resolver;
+	}
+
+}
