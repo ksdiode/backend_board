ec17fd248fad887c9d04deb1170a18682b73d0a5
gusukim <gusukim@gmail.com>
2023-12-04 16:01:25 +0900

    mybatis project setting

--- src/main/java/org/scoula/config/RootConfig.java
+++ src/main/java/org/scoula/config/RootConfig.java
@@ -1,8 +1,52 @@
 package org.scoula.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
+// @MapperScan(basePackages  = {})
 public class RootConfig {
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+//		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		config.setJdbcUrl("jdbc:mysql://localhost:3306/glory_db2");
+        config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        config.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/glory_db2");
+        config.setUsername("glory");
+        config.setPassword("1234");
+
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return dataSource;
+    }
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setConfigLocation(
+                applicationContext.getResource("classpath:/mybatis-config.xml"));
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+        DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
+        return manager;
+    }
 
 }
